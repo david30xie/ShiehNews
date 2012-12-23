b4c4282cd803cab17f7009cb9ab3cbd482b59205
Foina <1552988785@qq.com>
2012-12-23 16:50:51 +0800

    _articleFormeditor

--- shiehnews/protected/views/admin/article/_articleForm.php
+++ shiehnews/protected/views/admin/article/_articleForm.php
@@ -14,7 +14,15 @@
 
 <p>
 	<?php echo CHtml::activeLabelEx($form, 'content'); ?>
-	<?php echo CHtml::activeTextArea($form, 'content'); ?>
+	<?php $this->widget('application.extensions.editor.CKkceditor',array(
+			"model"=>$form,                # Data-Model
+			"attribute"=>'content',         # Attribute in the Data-Model
+			"height"=>'400px',
+			"width"=>'100%',
+			"filespath"=>Yii::app()->basePath."/../up/",
+			"filesurl"=>Yii::app()->baseUrl."/up/",
+		) );  ?>
+	<?php //echo CHtml::activeTextArea($form, 'content'); ?>
 </p>
 
 <p>
