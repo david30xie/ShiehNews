85ace55747e4b5eaade7c59125b2f1d495821956
Foina <1552988785@qq.com>
2013-07-02 22:50:02 +0800

    数据库

--- shiehnews/shiehnews.sql
+++ shiehnews/shiehnews.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.0.1.1
+-- version 3.3.9
 -- http://www.phpmyadmin.net
 --
--- 主机: localhost
--- 生成日期: 2009 年 08 月 04 日 16:07
--- 服务器版本: 5.0.51
--- PHP 版本: 5.2.9-1
+-- 主机: 127.0.0.1
+-- 生成日期: 2013 年 07 月 02 日 16:28
+-- 服务器版本: 5.5.8
+-- PHP 版本: 5.3.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -26,19 +26,19 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `articles` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `categoryId` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `createdTime` int(11) NOT NULL,
   `modifiedTime` int(11) NOT NULL,
   `content` text NOT NULL,
-  `viewNum` int(11) NOT NULL default '0',
-  `commentNum` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`id`),
+  `viewNum` int(11) NOT NULL DEFAULT '0',
+  `commentNum` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
   KEY `fk_articles_users` (`userId`),
   KEY `fk_articles_categories` (`categoryId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -47,10 +47,10 @@ CREATE TABLE IF NOT EXISTS `articles` (
 --
 
 CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -59,16 +59,56 @@ CREATE TABLE IF NOT EXISTS `categories` (
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `articleId` int(11) NOT NULL,
   `comment` text NOT NULL,
   `createdTime` int(11) NOT NULL,
   `modifiedTime` int(11) NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `fk_comments_articles` (`articleId`),
   KEY `fk_comments_users` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `mycounter`
+--
+
+CREATE TABLE IF NOT EXISTS `mycounter` (
+  `Counterid` int(11) NOT NULL,
+  `RecordDate` date NOT NULL,
+  `Counter` int(11) NOT NULL,
+  `address` varchar(15) NOT NULL,
+  PRIMARY KEY (`Counterid`),
+  UNIQUE KEY `Counterid` (`Counterid`),
+  UNIQUE KEY `address` (`address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tag`
+--
+
+CREATE TABLE IF NOT EXISTS `tag` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `frequency` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `upload`
+--
+
+CREATE TABLE IF NOT EXISTS `upload` (
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -77,16 +117,16 @@ CREATE TABLE IF NOT EXISTS `comments` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `articleNum` int(11) NOT NULL default '0',
-  `commentNum` int(11) NOT NULL default '0',
+  `articleNum` int(11) NOT NULL DEFAULT '0',
+  `commentNum` int(11) NOT NULL DEFAULT '0',
   `email` varchar(50) NOT NULL,
-  `nickname` varchar(50) default NULL,
+  `nickname` varchar(50) DEFAULT NULL,
   `createdTime` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- 限制导出的表
